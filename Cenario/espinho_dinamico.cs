84895933d8d4d7066185e7a798721fdc79f09089
lucas bittencourt rauch <lucas.bittencourtrauch@gmail.com>
2023-12-04 19:44:13 -0300

    espinho dinamico

---
+++ Cenario/espinho_dinamico.cs
@@ -0,0 +1,43 @@
+using Godot;
+using System;
+
+public partial class espinho_dinamico : Node3D
+{
+	// Called when the node enters the scene tree for the first time.
+	[Export] public bool active;
+	[Export] public double timeOut = 5;
+	private CollisionShape3D spikeColision;
+	private Node3D spikeMesh;
+	public override void _Ready()
+	{
+		spikeColision = GetNode<CollisionShape3D>("Espinho/spikes/Damage_Component/CollisionShape3D");
+		spikeMesh = GetNode<Node3D>("Espinho/spikes");
+		if (!active)
+		{
+			spikeColision.Disabled = true;
+			spikeMesh.Visible = false;
+		}
+		Timer timer = GetNode<Timer>("Timer");
+		timer.WaitTime = timeOut;
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+
+	public void _on_timer_timeout()
+	{
+		if (active)
+		{
+			active = false;
+			spikeColision.Disabled = true;
+			spikeMesh.Visible = false;
+		}
+		else{
+			active = true;
+			spikeColision.Disabled = false;
+			spikeMesh.Visible = true;
+		}
+	}
+}
